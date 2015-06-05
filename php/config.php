73fdc267ba995924b2a231903699445660c85ac6
yurippe <yurippenet@gmail.com>
2015-06-05 18:40:19 +0200

    fix

--- php/config.php
+++ php/config.php
@@ -8,8 +8,7 @@ $mysql_database = "carelesscourage";
 function getMySQLConnection($ErrorMessage="Database Error"){
 		global $mysql_username, $mysql_password, $mysql_host, $mysql_port, $mysql_database;
 		$con = new mysqli($mysql_host, $mysql_username, $mysql_password, $mysql_database, $mysql_port);
-		if($con->errno){die($ErrorMessage);}
-		if($con->get_warnings()){die($ErrorMessage);}
+		if($con->connect_errno){die($ErrorMessage);}
 		return $con;
 }
 ?>
