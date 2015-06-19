47dda9db3bc5722c2c072f6a1001382146165223
CarelessCourage <samuelmorkbednarz@gmail.com>
2015-06-19 21:27:53 +0200

    updates

--- javascrips/user_post.js
+++ javascrips/user_post.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
     //makes the post user page tabs above the featured post switch which is selected and what content to show
     $("#user-tabs li div").click(function () {
         $("#user-tabs li div").removeClass("selected");
-        $("#user-tabs li div").addClass("hidden");
+        $("#user-featured .f").addClass("hidden");
         $(this).removeClass("hidden");
         $(this).addClass("selected");
     });
